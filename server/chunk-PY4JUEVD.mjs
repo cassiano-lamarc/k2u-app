0f8e20bf5d11bbf1dc94b374b66ad2a2db1648ae
cassiano.lamarc <cassiano.lamarc@metanetsistemas.com.br>
2026-05-15 15:29:02 -0300

    Auto-generated commit

--- server/chunk-PY4JUEVD.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-T55IDOPT.mjs";var o=[{path:"login",loadComponent:()=>import("./chunk-FWZSYVRN.mjs").then(e=>e.LoginComponent),\u0275entryName:"src/app/features/auth/pages/login/login.component.ts"}];export{o as AUTH_ROUTES};
